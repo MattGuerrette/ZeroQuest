5c87a3eaacf4754fe067b6c23584ff6a8edebcc1
Matt Guerrette <direct3Dtutorials@gmail.com>
2015-11-03 19:40:27 -0500

    init commit

---
+++ ZQMain.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+import pygame
+import os;
+from gi.repository import Gtk
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+class ZQMain:
+    def __init__(self):
+        # Set up a clock for managing the frame rate.
+        self.clock = pygame.time.Clock()
+
+        self.paused = False
+
+
+    def set_paused(self, paused):
+        self.paused = paused
+
+    # Called to save the state of the game to the Journal.
+    def write_file(self, file_path):
+        pass
+
+    # Called to load the state of the game from the Journal.
+    def read_file(self, file_path):
+        pass
+
+    # The main game loop.
+    def run(self):
+        self.running = True
+
+        screen = pygame.display.get_surface()
+
+        while self.running:
+            # Pump GTK messages.
+            while Gtk.events_pending():
+                Gtk.main_iteration()
+
+            # Pump PyGame messages.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                elif event.type == pygame.VIDEORESIZE:
+                    pygame.display.set_mode(event.size, pygame.RESIZABLE)
+
+
+            # Clear Display
+            screen.fill((255, 255, 255))  # 255 for white
+
+            # Flip Display
+            pygame.display.flip()
+
+            # Try to stay at 30 FPS
+            self.clock.tick(30)
+
+
+# This function is called when the game is run directly from the command line:
+# ./TestGame.py
+def main():
+    pygame.init()
+    pygame.display.set_mode((800, 600), pygame.RESIZABLE)
+    game = ZQMain()
+    game.run()
+
+if __name__ == '__main__':
+    main()
